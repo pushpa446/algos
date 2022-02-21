b5a3b458417f1af3cf92df139924dd0d1edeb2e9
Pushpa <dumpalapushpalatha@gmail.com>
2022-02-21 22:48:12 +0530

    Rename file

--- binarySearch.java
+++ binarySearch.java
@@ -1,26 +1,26 @@
 class BinarySearch {
-	public static void main(String[] args) {
-		// input
-		int[] numbers = { 1, 2 };
-		int target = 2;
+    public static void main(String[] args) {
+        // input
+        int[] numbers = {1, 2};
+        int target = 2;
 
-		int start = 0;
-		int end = numbers.length - 1;
-		int mid;
-		int target_index = -1;
+        int start = 0;
+        int end = numbers.length - 1;
+        int mid;
+        int target_index = -1;
 
-		while (start <= end) {
-			mid = start + (end - start)/2;
-			if (target == numbers[mid]) {
-				target_index = mid;
-				break;
-			}
-			if (target < numbers[mid]) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
-			}
-		}
-		System.out.printf("target index is %d", target_index);
-	}
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (target == numbers[mid]) {
+                target_index = mid;
+                break;
+            }
+            if (target < numbers[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.printf("target index is %d", target_index);
+    }
 }
