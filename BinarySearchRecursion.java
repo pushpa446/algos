c7777188e03b3a26514fac796159c4200dd7ec93
pushpa446 <dumpalapushpalatha@gmail.com>
2022-02-21 19:08:15 +0530

    format the file

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -6,14 +6,14 @@ public class BinarySearchRecursion {
     }
 
     private static int search(int[] nums, int target, int start, int end) {
-        if(start >= end) {
+        if (start >= end) {
             return -1;
         }
         int mid = start + (end - start) / 2;
-        if(nums[mid] == target) {
+        if (nums[mid] == target) {
             return mid;
         }
-        if(target > nums[mid]) {
+        if (target > nums[mid]) {
             return search(nums, target, mid + 1, end);
         }
         return search(nums, target, start, mid - 1);
