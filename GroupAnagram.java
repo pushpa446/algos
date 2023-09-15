1172008bc69875e5a1d21f118e4c6133a1f68e63
Pushpalatha Dumpala <pdumpala@thoughtworks.com>
2023-09-15 20:59:12 +0530

    update group anagram

--- GroupAnagram.java
+++ GroupAnagram.java
@@ -8,7 +8,11 @@ public class GroupAnagram {
                 System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
 
         }
-
+/*
+sort each string ,if already exists append to the list else add it to map
+Time complexity O(n * mlogm)
+Space complexity O(n)
+ */
         public static List<List<String>> groupAnagrams(String[] strs) {
                 HashMap<String, List<String>> map = new HashMap<>();
                 for (String string :
@@ -16,14 +20,11 @@ public class GroupAnagram {
                         char[] chars = string.toCharArray();
                         Arrays.sort(chars);
                         String sortedString = Arrays.toString(chars);
-                        if(map.containsKey(sortedString)){
-                                List<String> strings = map.get(sortedString);
-                                strings.add(string);
-                        } else {
-                                map.put(sortedString, new ArrayList<>(Arrays.asList(string)));
+                        if(!map.containsKey(sortedString)){
+                                map.put(sortedString, new ArrayList<>());
                         }
+                        map.get(sortedString).add(string);
                 }
-                return map.values().stream().toList();
-
+                return new ArrayList<>(map.values());
         }
 }
