b329df2235a6b35fb8c3f132531371e3a8f12cbd
Pushpalatha Dumpala <pdumpala@thoughtworks.com>
2023-09-15 20:08:27 +0530

    max possible sum of sub array

--- MaxSumSubarray.java
+++ MaxSumSubarray.java
@@ -1,12 +1,12 @@
 public class MaxSumSubarray {
         public static void main(String[] args) {
-                System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+                System.out.println(maxSubArray(new int[]{-2, -3, -1, -4, -5}));
         }
 
         /*
         brute force
         take all possible subarrays and compare o(n^2)
-         */
+
         public static int maxSubArray(int[] nums) {
                 int maxSum = nums[0];
                 for (int i = 0; i < nums.length; i++) {
@@ -16,6 +16,26 @@ public class MaxSumSubarray {
                                 if (currentSum > maxSum) {
                                         maxSum = currentSum;
                                 }
+                        }
+                }
+                return maxSum;
+        }
+         */
+
+        /*
+        if sum so far is negative it does not contribute to the largest sum, so eliminate that subarray
+         */
+        public static int maxSubArray(int[] nums) {
+                int maxSum = nums[0];
+                int currentSum = 0;
+                for (int num : nums) {
+                        if(num > currentSum + num ){
+                                currentSum = num;
+                        } else {
+                                currentSum += num;
+                        }
+                        if (currentSum > maxSum) {
+                                maxSum = currentSum;
                         }
                 }
                 return maxSum;
