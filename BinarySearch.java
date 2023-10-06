d4e95a40c7ef2ef491fddb71950512c0febb8af9
Pushpalatha Dumpala <pdumpala@thoughtworks.com>
2023-10-06 16:10:02 +0530

    binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,13 @@
 class BinarySearch {
+        /*
+        this works on sorted asc order
+        adjusting the subarray to search
+        start mid end
+        if target is greater than mid -> start = mid + 1
+        if target is less than mid -> end = mid - 1
+        eventually you will reach the number
+        everytime you cut half of the array, so log n complexity
+         */
         public static void main(String[] args) {
                 // input
                 int[] numbers = {1, 2};
